c301e09c2a9b70f274a95001da2bc60ae2f266a4
Stanislav Machel <stanislav.machel@gmail.com>
2018-11-25 12:35:02 +0200

    The Spring Context

--- src/main/java/com/example/didemo/controllers/MyController.java
+++ src/main/java/com/example/didemo/controllers/MyController.java
@@ -1,4 +1,12 @@
 package com.example.didemo.controllers;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class MyController {
+    public String hello(){
+        System.out.println("Hello, World!");
+
+        return "foo";
+    }
 }
