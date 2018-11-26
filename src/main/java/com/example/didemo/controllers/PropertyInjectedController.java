985e6d797fb095c396ff85e5992335eaa5749ef1
Stanislav Machel <stanislav.machel@gmail.com>
2018-11-26 19:43:45 +0200

    Use Primary anotation to define default injected implementation for GreetingService

--- src/main/java/com/example/didemo/controllers/PropertyInjectedController.java
+++ src/main/java/com/example/didemo/controllers/PropertyInjectedController.java
@@ -3,12 +3,16 @@ package com.example.didemo.controllers;
 
 import com.example.didemo.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PropertyInjectedController {
 
     @Autowired
+    @Qualifier("greetingServiceImpl")
+    //if field name maches bean name Spring injects GreetingServiceImpl
+    //but here is needed qualifier because @Primary overrides this behaviour
     public GreetingService greetingServiceImpl;
 
     public String sayHello(){
